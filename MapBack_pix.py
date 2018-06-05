0dbad74f58899f041f972832d8506941828d6015
Arianna Renzini <pai@dyn1137-210.insecure.ic.ac.uk>
2018-06-05 11:50:03 +0100

    up

--- MapBack_pix.py
+++ MapBack_pix.py
@@ -47,6 +47,7 @@ def map_in_gauss(nside_in, noise_lvl):
     elif noise_lvl == 2: alpha = 1.e-38
     elif noise_lvl == 3: alpha = 3.e-39
     elif noise_lvl == 4: alpha = 5.e-40
+    elif noise_lvl == 5: alpha = 1.e-41
 
     
     lmax = nside/4
@@ -582,6 +583,8 @@ class Telescope(object):
         elif noise_lvl == 2: alpha = 1.e-38
         elif noise_lvl == 3: alpha = 3.e-39
         elif noise_lvl == 4: alpha = 5.e-40
+        elif noise_lvl == 5: alpha = 1.e-41
+        
         
         self.alpha = alpha
         
