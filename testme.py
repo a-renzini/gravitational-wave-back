4b413f636a6712ee550745df4c7056528ffdde9d
Arianna Renzini <pai@dyn3141-221.wlan.ic.ac.uk>
2018-11-06 18:03:15 +0000

    up

--- testme.py
+++ testme.py
@@ -22,20 +22,23 @@ from matplotlib import cm
 file = np.load('strain_h1.npz')
 
 h1 = file['strain']
-
 Nt = len(h1)
-dt = 1./4096
+h1 *= signal.tukey(Nt,alpha=0.05)
+
+dl = 1./4096
 low_f = 30.
 high_f = 500.
 
-freqs = np.fft.rfftfreq(2*Nt, dt)
+freqs = np.fft.rfftfreq(2*Nt, dl)
 freqs = freqs[:Nt/2+1]
 
-hf = np.fft.rfft(h1, n=Nt, norm = 'ortho') 
+hf = np.fft.rfft(h1, n=2*Nt, norm = 'ortho') 
+
+hf = hf[:Nt/2+1]
 
 print hf
 
-Pxx, frexx = mlab.psd(hf, Fs=4096, NFFT=2*4096,noverlap=4096/2,window=np.blackman(2*4096),scale_by_freq=False)
+Pxx, frexx = mlab.psd(h1, Fs=4096, NFFT=2*4096,noverlap=4096/2,window=np.blackman(2*4096),scale_by_freq=False)
 
 hf_psd = interp1d(frexx,Pxx)
 hf_psd_data = abs(hf.copy()*np.conj(hf.copy())) 
@@ -43,8 +46,16 @@ hf_psd_data = abs(hf.copy()*np.conj(hf.copy()))
 mask = (freqs>low_f) & (freqs < high_f)
 mask2 = (freqs>80.) & (freqs < 300.)
 
+plt.figure()
+plt.loglog(hf_psd_data[mask])
+plt.loglog(hf_psd(freqs)[mask])
+plt.savefig('compare.pdf')
+
+
 norm = np.mean(hf_psd_data[mask])/np.mean(hf_psd(freqs)[mask])
 norm2 = np.mean(hf_psd_data[mask2])/np.mean(hf_psd(freqs)[mask2])
+
+print norm, norm2
 
 np.savez('hf.npz', hf = hf,Pxx = Pxx, frexx = frexx, norm = norm, norm2 = norm2)
 
